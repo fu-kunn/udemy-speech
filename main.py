8ccf1dc5b4a6bab5923afaec0017a029d8488e64
fu-kunn <sky53908@gmail.com>
2023-03-17 16:43:36 +0900

    [Add]ipythonのインストール

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'secret.json'
 
 
 from google.cloud import texttospeech
+from IPython.display import Audio
 
 client = texttospeech.TextToSpeechClient()
 
-synthesis_input = texttospeech.SynthesisInput(text="Hello, World!")
+synthesis_input = texttospeech.SynthesisInput(text="こんにちは、私はプログラミング講師のふーくんです")
 
 voice = texttospeech.VoiceSelectionParams(
-    language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+    language_code="ja-JP", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
 )
 
 audio_config = texttospeech.AudioConfig(
@@ -22,7 +23,8 @@ response = client.synthesize_speech(
     input=synthesis_input, voice=voice, audio_config=audio_config
 )
 
-filename = "output.mp3"
-with open(filename, "wb") as out:
-    out.write(response.audio_content)
-    print(f"音声データは{filename}ファイルに書き出しました")
+Audio(response.audio_content)
+# filename = "output.mp3"
+# with open(filename, "wb") as out:
+#     out.write(response.audio_content)
+#     print(f"音声データは{filename}ファイルに書き出しました")
