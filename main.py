5f6577bf0392e027261004a257618d49807193da
fu-kunn <sky53908@gmail.com>
2023-03-20 09:51:55 +0900

    [Add]streamlitの実行を確認

--- main.py
+++ main.py
@@ -1,49 +1,72 @@
 import os
-
+import io
+import streamlit as st
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'secret.json'
 
 from google.cloud import texttospeech
-from IPython.display import Audio
+# from IPython.display import Audio
 
 
 # 関数の作成
-gender_type = {
-    'defalut': texttospeech.SsmlVoiceGender.SSML_VOICE_GENDER_UNSPECIFIED,
-    'male': texttospeech.SsmlVoiceGender.MALE,
-    'female': texttospeech.SsmlVoiceGender.FEMALE,
-    'neutral': texttospeech.SsmlVoiceGender.NEUTRAL
-}
-lang_code = {
-    '英語': 'en-US',
-    '日本語': 'ja-JP'
-}
+def synthesize_speech(text, lang='日本語', gender='defalut'):
+    gender_type = {
+        'defalut': texttospeech.SsmlVoiceGender.SSML_VOICE_GENDER_UNSPECIFIED,
+        'male': texttospeech.SsmlVoiceGender.MALE,
+        'female': texttospeech.SsmlVoiceGender.FEMALE,
+        'neutral': texttospeech.SsmlVoiceGender.NEUTRAL
+    }
 
-lang = '日本語'
-gender = 'defalut'
-text = "こんにちは、私はプログラミング講師のふーくんです"
+    lang_code = {
+        '英語': 'en-US',
+        '日本語': 'ja-JP'
+    }
+
+    client = texttospeech.TextToSpeechClient()
+
+    synthesis_input = texttospeech.SynthesisInput(text=text)
+
+    voice = texttospeech.VoiceSelectionParams(
+        language_code=lang_code[lang], ssml_gender=gender_type[gender]
+        # language_code=lang_code[lang], ssml_gender=texttospeech.SsmlVoiceGender.MALE
+    )
+
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3
+    )
+
+    # APIを叩いてresponseに入れている
+    response = client.synthesize_speech(
+        input=synthesis_input, voice=voice, audio_config=audio_config
+    )
+
+    return response
+
+st.title('音声出力アプリ')
 
 
 
 
-client = texttospeech.TextToSpeechClient()
 
-synthesis_input = texttospeech.SynthesisInput(text=text)
 
-voice = texttospeech.VoiceSelectionParams(
-    language_code=lang_code[lang], ssml_gender=gender_type[gender]
-)
 
-audio_config = texttospeech.AudioConfig(
-    audio_encoding=texttospeech.AudioEncoding.MP3
-)
+# lang = '日本語'
+# gender = 'defalut'
+# text = "こんにちは、私はプロ野球選手のふーくんです"
 
-# APIを叩いてresponseに入れている
-response = client.synthesize_speech(
-    input=synthesis_input, voice=voice, audio_config=audio_config
-)
+# """
+# 問題：
+# genderの引数を変えても声の性別が変わらない
+# 日本語➡︎女性
+# 英語　➡︎男性
+# """
 
-# Audio(response.audio_content)
-filename = "output.mp3"
-with open(filename, "wb") as out:
-    out.write(response.audio_content)
-    print(f"音声データは{filename}ファイルに書き出しました")
+
+# response = synthesize_speech(text, lang='日本語', gender='male')
+
+# # Audio(response.audio_content)
+# filename = "output.mp3"
+# with open(filename, "wb") as out:
+#     out.write(response.audio_content)
+#     print(f"音声データは{filename}ファイルに書き出しました")
+
+
